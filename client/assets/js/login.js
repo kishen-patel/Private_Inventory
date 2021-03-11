876e0b184ace8abbb04828d9ece6d0683bc7989f
kishen_patel <patel@controlled-chaos.co.za>
2021-03-11 18:12:01 +0200

    serverv1.1

--- client/assets/js/login.js
+++ client/assets/js/login.js
@@ -1,17 +1,55 @@
+var socket = io();
+
+function Authenticate() {
+  var loginCookieValue = getCookie("Login");
+  if (loginCookieValue == "true") {
+    window.location.replace("localhost:3000");
+  } else {
+    console.log("Not Logged In");
+  }
+}
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-    loginErrorMsg.style.opacity = 1;
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+  loginErrorMsg.style.opacity = 0;
+  socket.emit("Login", {
+    username: username,
+    password: password,
+  });
+});
 
-    // if (username === "user" && password === "web_dev") {
-    //     alert("You have successfully logged in.");
-    //     location.reload();
-    // } else {
-    //     loginErrorMsg.style.opacity = 1;
-    // }
-})
+socket.on("Verified", function (data) {
+  loginErrorMsg.style.opacity = 0;
+  setCookie("Login", "true", 1);
+  window.location.replace("localhost:3000");
+});
+socket.on("Failed", function (data) {
+  loginErrorMsg.style.opacity = 1;
+  setCookie("Login", "false", 1);
+});
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
--- server.js
+++ server.js
@@ -4,17 +4,24 @@
 
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
-const https = require("https");
+const ObjectId = require("mongodb").ObjectID;
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const uri =
   "mongodb+srv://314IT:Final314IT@cluster0.jd8k7.mongodb.net/Private_Inventory?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-});
 const app = express();
 const port = 3000;
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+let clientDB = null;
+
+//================================================================================
+// #endregion Server Variables
+//================================================================================
+
+//================================================================================
+// #region Server Config
+//================================================================================
 
 app.use(
   bodyParser.urlencoded({
@@ -30,6 +37,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/client/index.html");
 });
 
+app.get("/login", function (req, res) {
+  res.sendFile(__dirname + "/client/login.html");
+});
+
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
@@ -41,40 +52,228 @@ app.use(function (req, res, next) {
   next();
 });
 
-// MongoClient.connect(
-//   db.url,
-//   {
-//     useNewUrlParser: true,
-//   },
-//   function (err, client) {
-//     var rekDB = client.db("rek_api");
-//     if (err) throw err;
-//     require("./app/routes")(app, rekDB);
-//     https.createServer(options, app).listen(port, () => {
-//       console.log("We are live on " + port);
-//     });
-//     // app.listen(port, () => {
-//     //     console.log('We are live on ' + port);
-//     // });
-//   }
-// );
+//================================================================================
+// #endregion Server Config
+//================================================================================
 
-client.connect((err) => {
-  const collection = client.db("314IT").collection("Stock");
-  //   const stock = {
-  //     product_ID: 123,
-  //   };
-  //   collection.insertOne(stock, (err, result) => {
-  //     if (err) return console.error(err);
-  //     console.info(result);
-  //   });
-  app.listen(port, () => {
-    console.log("We are live on " + port);
-  });
-  // perform actions on the collection object
-  client.close();
+//================================================================================
+// #region Server Hosting
+//================================================================================
+
+http.listen(port, () => {
+  console.log("We are live on " + port);
 });
 
 //================================================================================
-// #endregion Server Variables
+// #endregion Server Hosting
+//================================================================================
+
+//================================================================================
+// #region Socket.IO
+//================================================================================
+
+io.on("connection", async function (socket) {
+  if ((clientDB && !clientDB.isConnected()) || clientDB === null) {
+    clientDB = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+  //check login details
+  socket.on("Login", async function (data) {
+    console.info(data);
+    try {
+      const db = clientDB.db("314IT");
+      const collection_users = db.collection("Users");
+      const checkUser = await collection_users.findOne(
+        {
+          username: data.username,
+        },
+        {
+          projection: {
+            _id: 1,
+            password: 1,
+          },
+        }
+      );
+      console.info(checkUser);
+      if (checkUser && checkUser.password == data.password) {
+        socket.emit("Verified", {});
+        socket.loggedIn = true;
+      } else {
+        console.log("invalid credentials");
+        socket.emit("Failed", {});
+        socket.loggedIn = false;
+      }
+    } catch (error) {
+      socket.emit("Error", { error: error });
+    }
+  });
+
+  //create finished product
+  socket.on("Create_Product", async function (data) {
+    console.info(data);
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_stock = db.collection("Stock");
+        const stock = {
+          Name: data.name,
+          Description: data.description,
+          Quantity: data.quantity,
+          Price: parseInt(data.price),
+        };
+        const stockInsert = await collection_stock.insertOne(
+          stock,
+          (err, result) => {
+            if (err) return console.error(err);
+            console.info(stockInsert.ops);
+            socket.emit("Product_Added", {
+              product: stock,
+            });
+            console.info(result);
+          }
+        );
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+
+  //get all finished products
+  socket.on("Get_Products", async function (data) {
+    console.info(data);
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_stock = db.collection("Stock");
+        const checkProduct = await collection_stock
+          .find({
+            date_added: {
+              $gte: new Date(new Date(data.startDate).setHours(00, 00, 00)),
+              $lt: new Date(new Date(data.endDate).setHours(23, 59, 59)),
+            },
+          })
+          .toArray();
+        //2020-02-12 date
+        console.info(checkProduct);
+        if (checkProduct) {
+          socket.emit("Got_Products", { products: checkProduct });
+        } else {
+          socket.emit("Error", { error: "Failed to get finished products" });
+        }
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+
+  //delete finished product
+  socket.on("Delete_Product", async function (data) {
+    console.info(data);
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_stock = db.collection("Stock");
+        const deleteProduct = await collection_stock.deleteOne(
+          {
+            _id: ObjectId(data.productID),
+          },
+          true
+        );
+        console.info(deleteProduct);
+        const checkProduct = await collection_stock.find({});
+        if (deleteProduct) {
+          socket.emit("Deleted_Product", { products: checkProduct });
+        } else {
+          socket.emit("Error", { error: "Failed to get finished products" });
+        }
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+
+  //create raw material
+  socket.on("Create_Raw_Material", async function (data) {
+    console.info(data);
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_raw = db.collection("Raw_Materials");
+        const raw = {
+          Name: data.name,
+          Description: data.description,
+          Quantity: data.quantity,
+          Price: parseInt(data.price),
+        };
+        const rawInsert = await collection_raw.insertOne(raw, (err, result) => {
+          if (err) return console.error(err);
+          console.info(rawInsert.ops);
+          socket.emit("Raw_Added", {
+            raw: raw,
+          });
+          console.info(result);
+        });
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+
+  //get all raw materials
+  socket.on("Get_Raw_Materials", async function (data) {
+    console.info(data);
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_raw = db.collection("Raw_Materials");
+        const checkRaw = await collection_raw.find({});
+        console.info(checkRaw);
+        if (checkRaw) {
+          socket.emit("Got_Raw_Materuaks", { raw_materials: checkRaw });
+        } else {
+          socket.emit("Error", { error: "Failed to get raw materials" });
+        }
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+
+  //delete raw material
+  socket.on("Delete_Raw_Material", async function (data) {
+    console.info(data);
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_raw = db.collection("Raw_Materials");
+        const deleteRaw = await collection_raw.deleteOne(
+          {
+            _id: ObjectId(data.rawID),
+          },
+          true
+        );
+        console.info(deleteRaw);
+        const checkRaw = await collection_raw.find({});
+        if (deleteProduct) {
+          socket.emit("Deleted_Product", { raw_materials: checkRaw });
+        } else {
+          socket.emit("Error", { error: "Failed to get finished products" });
+        }
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+});
+
+//================================================================================
+// #endregion Socket.IO
 //================================================================================
