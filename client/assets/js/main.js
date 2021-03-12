1d2bceab7a148a1cf385181e76ab63e24d300474
kishen_patel <patel@controlled-chaos.co.za>
2021-03-12 17:52:54 +0200

    1.4

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -13,6 +13,8 @@ function Dashboard_Click() {
 }
 
 function Inventory_Click() {
+  socket.emit("Get_Raw_Materials", {});
+  socket.emit("Get_Finished_Products", {});
   document.getElementById("dashboard").style.display = "none";
   document.getElementById("product_list").style.display = "none";
   document.getElementById("inventory_list").style.display = "block";
@@ -173,4 +175,138 @@ socket.on("Deleted_Product", function (data) {
   $("#deleteModal").modal("hide");
   $("#successModal").modal("show");
   RefreshProductTable(data);
+});
+
+function AddRaw() {
+  var rawMaterialSelect = document.getElementById("rawSelect").value;
+  var rawMaterialAmount = document.getElementById("rawAmount").value;
+  socket.emit("Add_Raw_Material", {
+    Raw_Material: rawMaterialSelect,
+    Quantity: rawMaterialAmount,
+  });
+}
+
+socket.on("Raw_Added", function (data) {
+  $("#raw-info")[0].reset();
+  $("#rawModal").modal("hide");
+  $("#successModal").modal("show");
+  RefreshRawTable(data);
+});
+
+function RefreshRawTable(data) {
+  var table = document.getElementById("raw-table");
+  var totalRowCount = table.tBodies[0].rows.length;
+  for (let index = 0; index < totalRowCount; index++) {
+    document
+      .getElementById("raw-tbody")
+      .removeChild(document.getElementById("raw-tbody").childNodes[0]);
+  }
+  var tbdy = document.getElementById("raw-tbody");
+  for (let i = 0; i < data.raw.length; i++) {
+    var tr = document.createElement("tr");
+    var tdID = document.createElement("td");
+    var tdName = document.createElement("td");
+    var tdQuantity = document.createElement("td");
+    var tdPrice = document.createElement("td");
+    var tdTotalPrice = document.createElement("td");
+    // var tdDelete = document.createElement("td");
+
+    tdID.innerHTML = data.raw[i]._id;
+    tdID.setAttribute("class", "raw_ID");
+    tdName.innerHTML = data.raw[i].Name;
+    tdPrice.innerHTML = data.raw[i].Price;
+    tdQuantity.innerHTML = data.raw[i].Quantity;
+    tdTotalPrice.innerHTML =
+      parseFloat(data.raw[i].Quantity) * parseFloat(data.raw[i].Price);
+    // tdDelete.innerHTML =
+    // "<button type='button' rel='tooltip' class='btn btn-danger text-right' onclick='DeleteRaw(this)' id='deleteRaw' data-toggle='modal' data-target='#deleteRawModal'><i class='material-icons'>delete</i></button>";
+
+    tr.appendChild(tdID);
+    tr.appendChild(tdName);
+    tr.appendChild(tdQuantity);
+    tr.appendChild(tdPrice);
+    tr.appendChild(tdTotalPrice);
+    // tr.appendChild(tdDelete);
+
+    tbdy.appendChild(tr);
+  }
+}
+
+socket.on("Got_Raw_Materials", function (data) {
+  RefreshRawTable(data);
+});
+
+function AddFinishedList() {
+  socket.emit("Get_Product_List", {});
+}
+
+socket.on("Got_Product_List", function (data) {
+  var options = document.querySelectorAll("#finishedOption");
+  options.forEach((o) => o.remove());
+  $("#finished-info")[0].reset();
+
+  for (let i = 0; i < data.Product_List.length; i++) {
+    var finishedSelect = document.getElementById("finishedSelect");
+    var options = document.createElement("option");
+    options.setAttribute("id", "finishedOption");
+    options.text = data.Product_List[i].Product_Name;
+    finishedSelect.add(options);
+  }
+});
+
+function AddFinished() {
+  var finishedSelect = document.getElementById("finishedSelect").value;
+  var finishedAmount = document.getElementById("finishedAmount").value;
+  socket.emit("Add_Finished_Product", {
+    Finished_Product: finishedSelect,
+    Quantity: finishedAmount,
+  });
+}
+
+socket.on("Finished_Product_Added", function (data) {
+  $("#finished-info")[0].reset();
+  $("#finishedModal").modal("hide");
+  $("#successModal").modal("show");
+  Inventory_Click();
+});
+
+function RefreshFinishedTable(data) {
+  var table = document.getElementById("finished-table");
+  var totalRowCount = table.tBodies[0].rows.length;
+  for (let index = 0; index < totalRowCount; index++) {
+    document
+      .getElementById("finished-tbody")
+      .removeChild(document.getElementById("finished-tbody").childNodes[0]);
+  }
+  var tbdy = document.getElementById("finished-tbody");
+  for (let i = 0; i < data.stock.length; i++) {
+    var tr = document.createElement("tr");
+    var tdID = document.createElement("td");
+    var tdName = document.createElement("td");
+    var tdDescription = document.createElement("td");
+    var tdPrice = document.createElement("td");
+    var tdQuantity = document.createElement("td");
+
+    tdID.innerHTML = data.stock[i]._id;
+    tdID.setAttribute("class", "finished_ID");
+    tdName.innerHTML = data.stock[i].Name;
+    tdDescription.innerHTML = data.stock[i].Description;
+    tdQuantity.innerHTML = data.stock[i].Quantity;
+    tdPrice.innerHTML = data.stock[i].Price;
+    // tdDelete.innerHTML =
+    //   "<button type='button' rel='tooltip' class='btn btn-danger text-right' onclick='DeleteProduct(this)' id='deleteProduct' data-toggle='modal' data-target='#deleteModal'><i class='material-icons'>delete</i></button>";
+
+    tr.appendChild(tdID);
+    tr.appendChild(tdName);
+    tr.appendChild(tdDescription);
+    tr.appendChild(tdQuantity);
+    tr.appendChild(tdPrice);
+    // tr.appendChild(tdDelete);
+
+    tbdy.appendChild(tr);
+  }
+}
+
+socket.on("Got_Finished_Products", function (data) {
+  RefreshFinishedTable(data);
 });
--- server.js
+++ server.js
@@ -172,29 +172,308 @@ io.on("connection", async function (socket) {
     }
   });
 
+  //get product list
+  socket.on("Get_Product_List", async function (data) {
+    console.log("ran");
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_product = db.collection("Products");
+        const productGet = await collection_product.find({}).toArray();
+        socket.emit("Got_Product_List", {
+          Product_List: productGet,
+        });
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+
   //create finished product
-  socket.on("Create_Finished_Product", async function (data) {
+  socket.on("Add_Finished_Product", async function (data) {
     console.info(data);
     console.log(socket.loggedIn);
     if ((socket.loggedIn = true)) {
       try {
         const db = clientDB.db("314IT");
         const collection_stock = db.collection("Stock");
-        const stock = {
-          Name: data.name,
-          Description: data.description,
-          Quantity: data.quantity,
-          Price: parseInt(data.price),
-        };
-        const stockInsert = await collection_stock.insertOne(
-          stock,
-          (err, result) => {
+        const collection_products = db.collection("Products");
+        const collection_raw = db.collection("Raw_Materials");
+        const collection_prod = db.collection("Production");
+        const getProduct = await collection_products.findOne({
+          Product_Name: data.Finished_Product,
+        });
+        var rawMaterials = getProduct.Raw_Materials;
+        var foam = parseFloat(rawMaterials.Foam) * parseFloat(data.Quantity);
+        var material =
+          parseFloat(rawMaterials.Material) * parseFloat(data.Quantity);
+        var tape = parseFloat(rawMaterials.Tape) * parseFloat(data.Quantity);
+        var plastic =
+          parseFloat(rawMaterials.Plastic) * parseFloat(data.Quantity);
+        var spun_bond =
+          parseFloat(rawMaterials.Spun_Bond) * parseFloat(data.Quantity);
+        var batting =
+          parseFloat(rawMaterials.Batting) * parseFloat(data.Quantity);
+        var spring =
+          parseFloat(rawMaterials.Spring) * parseFloat(data.Quantity);
+
+        var newFoam = 0;
+        var newMaterial = 0;
+        var newTape = 0;
+        var newPlastic = 0;
+        var newSpun = 0;
+        var newBatting = 0;
+        var newSpring = 0;
+
+        var newFoamV = false;
+        var newMaterialV = false;
+        var newTapeV = false;
+        var newPlasticV = false;
+        var newSpunV = false;
+        var newBattingV = false;
+        var newSpringV = false;
+
+        const rawGet = await collection_raw.find({}).toArray();
+        for (let i = 0; i < rawGet.length; i++) {
+          const rawMaterialName = rawGet[i].Name;
+          const rawMaterialQuantity = rawGet[i].Quantity;
+          if (
+            rawMaterialName == "Foam" &&
+            parseFloat(rawMaterialQuantity) > foam
+          ) {
+            newFoam = parseFloat(rawMaterialQuantity) - foam;
+            const rawUpdate = await collection_raw.updateOne(
+              {
+                Name: rawMaterialName,
+              },
+              {
+                $set: {
+                  Quantity: newFoam,
+                },
+              },
+              async function (err, result) {
+                if (err) return console.error(err);
+                newFoamV = true;
+                // console.log("Updated");
+              }
+            );
+          }
+          // else {
+          //   socket.emit("Error", {
+          //     error: "Not enough raw materials to make this product",
+          //   });
+          //   break;
+          // }
+          if (
+            rawMaterialName == "Material" &&
+            parseFloat(rawMaterialQuantity) > material
+          ) {
+            newMaterial = parseFloat(rawMaterialQuantity) - material;
+            const rawUpdate = await collection_raw.updateOne(
+              {
+                Name: rawMaterialName,
+              },
+              {
+                $set: {
+                  Quantity: newMaterial,
+                },
+              },
+              async function (err, result) {
+                if (err) return console.error(err);
+                newMaterialV = true;
+                // console.log("Updated");
+              }
+            );
+          }
+          // else {
+          //   socket.emit("Error", {
+          //     error: "Not enough raw materials to make this product",
+          //   });
+          //   break;
+          // }
+          if (
+            rawMaterialName == "Tape" &&
+            parseFloat(rawMaterialQuantity) > tape
+          ) {
+            newTape = parseFloat(rawMaterialQuantity) - tape;
+            const rawUpdate = await collection_raw.updateOne(
+              {
+                Name: rawMaterialName,
+              },
+              {
+                $set: {
+                  Quantity: newTape,
+                },
+              },
+              async function (err, result) {
+                if (err) return console.error(err);
+                newTapeV = true;
+                // console.log("Updated");
+              }
+            );
+          }
+          // else {
+          //   socket.emit("Error", {
+          //     error: "Not enough raw materials to make this product",
+          //   });
+          //   break;
+          // }
+          if (
+            rawMaterialName == "Plastic" &&
+            parseFloat(rawMaterialQuantity) > plastic
+          ) {
+            newPlastic = parseFloat(rawMaterialQuantity) - plastic;
+            const rawUpdate = await collection_raw.updateOne(
+              {
+                Name: rawMaterialName,
+              },
+              {
+                $set: {
+                  Quantity: newPlastic,
+                },
+              },
+              async function (err, result) {
+                if (err) return console.error(err);
+                newPlasticV = true;
+                // console.log("Updated");
+              }
+            );
+          }
+          // else {
+          //   socket.emit("Error", {
+          //     error: "Not enough raw materials to make this product",
+          //   });
+          //   break;
+          // }
+          if (
+            rawMaterialName == "Spun_Bond" &&
+            parseFloat(rawMaterialQuantity) > spun_bond
+          ) {
+            newSpun = parseFloat(rawMaterialQuantity) - spun_bond;
+            const rawUpdate = await collection_raw.updateOne(
+              {
+                Name: rawMaterialName,
+              },
+              {
+                $set: {
+                  Quantity: newSpun,
+                },
+              },
+              async function (err, result) {
+                if (err) return console.error(err);
+                newSpunV = true;
+                // console.log("Updated");
+              }
+            );
+          }
+          // else {
+          //   socket.emit("Error", {
+          //     error: "Not enough raw materials to make this product",
+          //   });
+          //   break;
+          // }
+          if (
+            rawMaterialName == "Batting" &&
+            parseFloat(rawMaterialQuantity) > batting
+          ) {
+            newBatting = parseFloat(rawMaterialQuantity) - batting;
+            const rawUpdate = await collection_raw.updateOne(
+              {
+                Name: rawMaterialName,
+              },
+              {
+                $set: {
+                  Quantity: newBatting,
+                },
+              },
+              async function (err, result) {
+                if (err) return console.error(err);
+                newBattingV = true;
+                // console.log("Updated");
+              }
+            );
+          }
+          // else {
+          //   socket.emit("Error", {
+          //     error: "Not enough raw materials to make this product",
+          //   });
+          //   break;
+          // }
+          if (
+            rawMaterialName == "Spring" &&
+            parseFloat(rawMaterialQuantity) > spring
+          ) {
+            newSpring = parseFloat(rawMaterialQuantity) - spring;
+            const rawUpdate = await collection_raw.updateOne(
+              {
+                Name: rawMaterialName,
+              },
+              {
+                $set: {
+                  Quantity: newSpring,
+                },
+              },
+              async function (err, result) {
+                if (err) return console.error(err);
+                newSpringV = true;
+                // console.log("Updated");
+              }
+            );
+          }
+          // else {
+          //   socket.emit("Error", {
+          //     error: "Not enough raw materials to make this product",
+          //   });
+          //   break;
+          // }
+        }
+
+        // if (
+        //   newFoamV == true &&
+        //   newMaterialV == true &&
+        //   newTapeV == true &&
+        //   newPlasticV == true &&
+        //   newSpunV == true &&
+        //   newSpringV == true &&
+        //   newBattingV == true
+        // ) {
+        // console.log("Ran stock update");
+
+        const stockProduct = await collection_stock.findOne({
+          Name: getProduct.Product_Name,
+        });
+
+        const finishedUpdate = await collection_stock.updateOne(
+          {
+            Name: getProduct.Product_Name,
+          },
+          {
+            $set: {
+              Quantity:
+                parseFloat(stockProduct.Quantity) + parseFloat(data.Quantity),
+            },
+          },
+          async function (err, result) {
             if (err) return console.error(err);
-            console.info(stockInsert.ops);
-            socket.emit("Finished_Product_Added", {
-              product: stock,
-            });
-            console.info(result);
+            if (result) {
+              // console.log("Ran product update");
+              const product = {
+                Name: getProduct.Product_Name,
+                Quantity: parseFloat(data.Quantity),
+                Date: Date.now(),
+              };
+              const productInsert = await collection_prod.insertOne(
+                product,
+                async function (err, result) {
+                  if (err) return socket.emit("Error", { error: err });
+                  const checkStock = await collection_stock.find({}).toArray();
+                  socket.emit("Finished_Product_Added", {
+                    stock: checkStock,
+                  });
+                }
+              );
+            }
           }
         );
       } catch (error) {
@@ -203,8 +482,25 @@ io.on("connection", async function (socket) {
     }
   });
 
-  //get all finished products
+  //read finished products
   socket.on("Get_Finished_Products", async function (data) {
+    console.log(socket.loggedIn);
+    if ((socket.loggedIn = true)) {
+      try {
+        const db = clientDB.db("314IT");
+        const collection_stock = db.collection("Stock");
+        const stockGet = await collection_stock.find({}).toArray();
+        socket.emit("Got_Finished_Products", {
+          stock: stockGet,
+        });
+      } catch (error) {
+        socket.emit("Error", { error: error });
+      }
+    }
+  });
+
+  //get all finished products
+  socket.on("Get_Finished_Products_Date", async function (data) {
     console.info(data);
     console.log(socket.loggedIn);
     if ((socket.loggedIn = true)) {
@@ -259,28 +555,46 @@ io.on("connection", async function (socket) {
     }
   });
 
-  //create raw material
-  socket.on("Create_Raw_Material", async function (data) {
+  //add raw materials
+  socket.on("Add_Raw_Material", async function (data) {
     console.info(data);
     console.log(socket.loggedIn);
     if ((socket.loggedIn = true)) {
       try {
         const db = clientDB.db("314IT");
         const collection_raw = db.collection("Raw_Materials");
-        const raw = {
-          Name: data.name,
-          Description: data.description,
-          Quantity: data.quantity,
-          Price: parseInt(data.price),
-        };
-        const rawInsert = await collection_raw.insertOne(raw, (err, result) => {
-          if (err) return console.error(err);
-          console.info(rawInsert.ops);
-          socket.emit("Raw_Added", {
-            raw: raw,
-          });
-          console.info(result);
+        const rawFind = await collection_raw.findOne({
+          Name: data.Raw_Material,
         });
+        var newQuantity =
+          parseFloat(rawFind.Quantity) + parseFloat(data.Quantity);
+        const raw = {
+          Name: data.Raw_Material,
+          Quantity: data.Amount,
+        };
+        const rawInsert = await collection_raw.updateOne(
+          {
+            Name: rawFind.Name,
+          },
+          {
+            $set: {
+              Quantity: newQuantity,
+            },
+          },
+          async function (err, result) {
+            if (err) return console.error(err);
+            if (result) {
+              const checkRaw = await collection_raw.find({}).toArray();
+              socket.emit("Raw_Added", {
+                raw: checkRaw,
+              });
+            } else {
+              socket.emit("Error", {
+                error: "Failed to get raw materials",
+              });
+            }
+          }
+        );
       } catch (error) {
         socket.emit("Error", { error: error });
       }
@@ -295,10 +609,9 @@ io.on("connection", async function (socket) {
       try {
         const db = clientDB.db("314IT");
         const collection_raw = db.collection("Raw_Materials");
-        const checkRaw = await collection_raw.find({});
-        console.info(checkRaw);
+        const checkRaw = await collection_raw.find({}).toArray();
         if (checkRaw) {
-          socket.emit("Got_Raw_Materuaks", { raw_materials: checkRaw });
+          socket.emit("Got_Raw_Materials", { raw: checkRaw });
         } else {
           socket.emit("Error", { error: "Failed to get raw materials" });
         }
